e3c37f42d2e38f22a7abae0d9121ce13439b2a9e
Chonsawat <chonsawat.nakanam@kkumail.com>
2024-12-26 19:10:34 +0000

    add gitsign

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,7 +3,6 @@ return {
     config = function() 
         vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {desc="Toggle [E]xplorer"})
         vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFocus<CR>", {desc="F[o]cus Explorer"})
-        vim.keymap.set("n", "<leader>O", "<cmd>buffer<CR>", {desc="Focus Buffer"})
         require("nvim-tree").setup({
             hijack_netrw =  true,
             auto_reload_on_write = true,
