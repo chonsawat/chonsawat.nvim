f717726f1a6640ab44823bf6ce26f2133ca987fa
Chonsawat <chonsawat.nakanam@kkumail.com>
2024-12-27 17:38:13 +0000

    add floatTerm

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -1,10 +1,10 @@
 return {
     "voldikss/vim-floaterm",
     config = function()
-       vim.keymap.set('n', '<A-t>n', ':FloatermNew<cr>', {desc="Floating Terminal [N]ew"})
-       vim.keymap.set('n', '<A-t>]', ':FloatermNext<cr>', {desc="Floating Terminal [N]ext"})
-       vim.keymap.set('n', '<A-t>[', ':FloatermPrev<cr>', {desc="Floating Terminal [P]revious"})
-       vim.keymap.set('n', '<A-\\>', ':FloatermToggle<cr>', {desc="Floating Terminal [T]oggle"})
-       vim.keymap.set('n', '<A-t>k', ':FloatermKill<cr>', {desc="Floating Terminal [K]ill"})
+       vim.keymap.set({'n', 't'}, '<A-t>n', '<cmd>FloatermNew<CR>', {desc="Floating Terminal [N]ew"})
+       vim.keymap.set({'n', 't'}, '<A-]>', '<cmd>FloatermNext<CR>', {desc="Floating Terminal [N]ext"})
+       vim.keymap.set({'n', 't'}, '<A-[>', '<cmd>FloatermPrev<CR>', {desc="Floating Terminal [P]revious"})
+       vim.keymap.set({'n', 't'}, '<A-\\>', '<cmd>FloatermToggle<CR>', {desc="Floating Terminal [T]oggle"})
+       vim.keymap.set({'n', 't'}, '<A-t>k', '<cmd>FloatermKill<CR>', {desc="Floating Terminal [K]ill"})
     end
 }
