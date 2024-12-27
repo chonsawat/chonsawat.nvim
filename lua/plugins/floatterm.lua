12091e0d4b5dbf7ddd6db4aa21827a121beff5ad
Chonsawat <chonsawat.nakanam@kkumail.com>
2024-12-27 17:51:50 +0000

    add noice and keymmap floatterm

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -1,10 +1,10 @@
 return {
     "voldikss/vim-floaterm",
     config = function()
-       vim.keymap.set({'n', 't'}, '<A-t>n', '<cmd>FloatermNew<CR>', {desc="Floating Terminal [N]ew"})
+       vim.keymap.set({'t'}, '<A-n>', '<cmd>FloatermNew<CR>', {desc="Floating Terminal [N]ew"})
        vim.keymap.set({'n', 't'}, '<A-]>', '<cmd>FloatermNext<CR>', {desc="Floating Terminal [N]ext"})
        vim.keymap.set({'n', 't'}, '<A-[>', '<cmd>FloatermPrev<CR>', {desc="Floating Terminal [P]revious"})
        vim.keymap.set({'n', 't'}, '<A-\\>', '<cmd>FloatermToggle<CR>', {desc="Floating Terminal [T]oggle"})
-       vim.keymap.set({'n', 't'}, '<A-t>k', '<cmd>FloatermKill<CR>', {desc="Floating Terminal [K]ill"})
+       vim.keymap.set({'t'}, '<A-k>', '<cmd>FloatermKill<CR>', {desc="Floating Terminal [K]ill"})
     end
 }
