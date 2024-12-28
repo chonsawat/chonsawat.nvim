1522d19c1dc3fa45e1c17f0f3a806b3e444a588e
Chonsawat <chonsawat.nakanam@kkumail.com>
2024-12-28 20:30:20 +0000

    Modify keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,7 +26,19 @@ vim.keymap.set({"v", "n"}, ",,", ":cprevious<cr>", {desc = "Quick list [P]reviou
 vim.keymap.set({"n"}, "<leader>sv", ":source $MYVIMRC<CR>", {desc = "Reload config nvim"})
 vim.keymap.set({"n", "v", "i"}, "<C-s>", "<cmd>w!<CR>", {desc = "Reload config nvim"})
 
+-- Buffer
+vim.keymap.set({"n"}, "th", "<cmd>bprev<enter>", {noremap = false})
+vim.keymap.set({"n"}, "tl", "<cmd>bnext<enter>", {noremap = false})
+vim.keymap.set({"n"}, "tk", "<cmd>bw!<enter>", {noremap = false})
+
 -- Required Noice and Telescope
 vim.keymap.set({"n"}, "<leader>st", function()
     require('telescope').extensions.notify.notify()
 end, {desc = "Reload config nvim"})
+
+-- Git Keymap
+-- Required Telescope
+vim.keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<CR>', {desc = "Git commit list", noremap = false})
+vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<CR>', {desc = "Git track file list", noremap = false})
+vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_stash<CR>', {desc = "Git stash list", noremap = false})
+vim.keymap.set('n', '<leader>gt', '<cmd>Telescope git_status<CR>', {desc = "Git status list", noremap = false})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,44 @@ return {
     "catppuccin/nvim",
     lazy = false,
     priority = 1000,
-    config = function () 
+    opts = {
+        no_italic = true,
+        term_colors = true,
+        transparent_background = true,
+        color_overrides = {
+            mocha = {
+                rosewater = "#F5B8AB",
+                flamingo = "#F29D9D",
+                pink = "#AD6FF7",
+                mauve = "#FF8F40",
+                red = "#E66767",
+                maroon = "#EB788B",
+                peach = "#FAB770",
+                yellow = "#FACA64",
+                green = "#70CF67",
+                teal = "#4CD4BD",
+                sky = "#61BDFF",
+                sapphire = "#4BA8FA",
+                blue = "#00BFFF",
+                lavender = "#00BBCC",
+                text = "#C1C9E6",
+                subtext1 = "#A3AAC2",
+                subtext0 = "#8E94AB",
+                overlay2 = "#7D8296",
+                overlay1 = "#676B80",
+                overlay0 = "#464957",
+                surface2 = "#3A3D4A",
+                surface1 = "#2F313D",
+                surface0 = "#1D1E29",
+                base = "#0b0b12",
+                mantle = "#11111a",
+                crust = "#191926",
+            },
+
+        }
+    },
+    config = function ()
         vim.cmd.colorscheme "catppuccin-mocha"
-    end
+        require('catppuccin').setup(opts)
+    end,
 }
