a245e3a38763dce80ee9735647568bd51804cd24
Chonsawat <chonsawat.nakanam@kkumail.com>
2024-12-28 21:22:54 +0000

    Modify Mapping v2

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
+
+-- Main Keymap
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -11,20 +20,43 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {desc = "Move foucus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", {desc = "Move foucus to the lower window"})
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", {desc = "Move foucus to the upper window"})
 
+-- Resize window
+Map("n", "<C-Up>", ":resize -2<CR>")
+Map("n", "<C-Down>", ":resize +2<CR>")
+Map("n", "<C-Left>", ":vertical resize -2<CR>")
+Map("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Resize terminal
+Map("t", "<C-Up>", "<cmd>resize -2<CR>")
+Map("t", "<C-Down>", "<cmd>resize +2<CR>")
+Map("t", "<C-Left>", "<cmd>vertical resize -2<CR>")
+Map("t", "<C-Right>", "<cmd>vertical resize +2<CR>")
+
+-- Diagnostic
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {desc = "Diagnostic goto [Prev]"})
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {desc = "Diagnostic goto [Next]"})
 
+-- Split
 vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", {desc = "[W]indow Split [V]ertical"})
 vim.keymap.set("n", "<leader>wh", ":split<cr>", {desc = "[W]indow Split [H]orizontal"})
 
+-- Indent
 vim.keymap.set("v", "<", "<gv", {desc = "Indent left in visual mode"})
 vim.keymap.set("v", ">", ">gv", {desc = "Indent right in visual mode"})
 
-vim.keymap.set({"v", "n"}, "..", ":cnext<cr>", {desc = "Quick list [N]ext"})
-vim.keymap.set({"v", "n"}, ",,", ":cprevious<cr>", {desc = "Quick list [P]revious"})
+-- Quick List
+vim.keymap.set({"v", "n"}, "<c-.>", ":cnext<cr>", {desc = "Quick list [N]ext"})
+vim.keymap.set({"v", "n"}, "<C-,>", ":cprevious<cr>", {desc = "Quick list [P]revious"})
 
+-- Reload nvim config
 vim.keymap.set({"n"}, "<leader>sv", ":source $MYVIMRC<CR>", {desc = "Reload config nvim"})
-vim.keymap.set({"n", "v", "i"}, "<C-s>", "<cmd>w!<CR>", {desc = "Reload config nvim"})
+
+-- Save file (force)
+vim.keymap.set({"n", "v", "i"}, "<C-s>", "<cmd>w!<CR>", {desc = "Save file (force)"})
+
+-- Move group of text
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Buffer
 vim.keymap.set({"n"}, "th", "<cmd>bprev<enter>", {noremap = false})
@@ -36,9 +68,5 @@ vim.keymap.set({"n"}, "<leader>st", function()
     require('telescope').extensions.notify.notify()
 end, {desc = "Reload config nvim"})
 
--- Git Keymap
--- Required Telescope
-vim.keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<CR>', {desc = "Git commit list", noremap = false})
-vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<CR>', {desc = "Git track file list", noremap = false})
-vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_stash<CR>', {desc = "Git stash list", noremap = false})
-vim.keymap.set('n', '<leader>gt', '<cmd>Telescope git_status<CR>', {desc = "Git status list", noremap = false})
+
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,13 @@ return {
             vim.keymap.set('n', '<leader>fr', builtin.resume, {desc="[F]ind [R]esume"})
             vim.keymap.set('n', '<leader>f.', builtin.oldfiles, {desc='[F]ind Repeat Files ("." for repeat)'})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc="[F]ind Existing [B]uffers"})
+            vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {desc="Color Scheme"})
+
+            -- Git Keymap
+            vim.keymap.set('n', '<leader>gC', '<cmd>Telescope git_commits<CR>', {desc = "Git commit list", noremap =true})
+            vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<CR>', {desc = "Git track file list", noremap = false})
+            vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_stash<CR>', {desc = "Git stash list", noremap = false})
+            vim.keymap.set('n', '<leader>gt', '<cmd>Telescope git_status<CR>', {desc = "Git status list", noremap = false})
 
         end
     }, 
