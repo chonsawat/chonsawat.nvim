78ee332a3e756791bacaa087b7926f7dc9325256
Chonsawat <chonsawat.nakanam@kkumail.com>
2024-12-31 18:43:34 +0000

    restructure for module

---
+++ lua/modules/ui/config.lua
@@ -0,0 +1,22 @@
+local config = {}
+
+function config.cyberdream()
+    require("cyberdream").setup({
+        transparent = true,
+        italic_comments = false,
+        borderless_telescope = false,
+        cache = true,
+        extensions = {
+            telescope = true,
+            notify = true,
+            mini = true,
+        },
+        options = {
+            colorscheme = "cyberdream"
+        }
+    })
+
+    vim.cmd.colorscheme "cyberdream"
+end
+
+return config
---
+++ lua/plugins/package.lua
@@ -0,0 +1,1 @@
+return require("modules.ui.package")
