6fbf9821cec533c3f0d7629c3193916117fdaae8
Chonsawat <chonsawat.nakanam@kkumail.com>
2025-01-01 18:02:44 +0000

    Add telescope macro

---
+++ lua/config/telescope-macros.lua
@@ -0,0 +1,3 @@
+require("telescope").load_extension "macros"
+
+vim.keymap.set("n", "<space>m", "<cmd>Telescope macros<cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -278,9 +278,9 @@ return {
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
             --- The below dependencies are optional,
-            "hrsh7th/nvim-cmp",    -- autocompletion for avante commands and mentions
+            "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
             "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            "zbirenbaum/copilot.lua",      -- for providers='copilot'
             {
                 -- support for image pasting
                 "HakonHarnes/img-clip.nvim",
@@ -308,4 +308,10 @@ return {
             },
         },
     },
+    {
+        "1riz/telescope-macros.nvim",
+        config = function()
+            require "config.telescope-macros"
+        end
+    }
 }
